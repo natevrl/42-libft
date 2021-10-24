8704282ac4e87528588e21a432e991a969f5e267
v3r <v3rslor@protonmail.com>
2021-10-24 22:55:35 +0200

    finit 42%, sur memccpy

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -2,6 +2,10 @@
 
 void	*ft_memcpy(void *dst, const void *src, size_t n)
 {
+	if (!dst && !src)
+		return (NULL);
+	if (n == 0)
+		return (dst);
 	while (n != 0)
 	{
 		((char*)dst)[n - 1] = ((const char*)src)[n - 1];
@@ -14,8 +18,10 @@ void	*ft_memcpy(void *dst, const void *src, size_t n)
 #include <string.h>
 int main()
 {
-	char str[] = "12345";
-	char dest[] = "1111111111";
-	printf("%s\n", ft_memcpy(dest, str, 0));
-	printf("%s\n", memcpy(dest, str, 0));
+	char str[100] = "12345";
+	char dest[100] = "0000000";
+	printf("%s\n", ft_memcpy(dest, str, 4));
+	char str2[100] = "abcde";
+	char dest2[100] = "0000000";
+	printf("%s\n", memcpy(dest2, str2, 2));
 }
