c5732b2f36d2a9c87aff71a3726b4347f3cd6e2c
Nathan BENHADOUCHE <nbenhado@e2r1p12.clusters.42paris.fr>
2021-11-24 16:43:31 +0100

    toujours pas compris memmove vs memcpy

--- ft_memchr.c
+++ ft_memchr.c
@@ -1,4 +1,42 @@
 #include "libft.h"
 
 
-void * memchr( const void * memoryBlock, int searchedChar, size_t size )
+void * ft_memchr( const void * memoryBlock, int searchedChar, size_t size )
+{
+	int i;
+	char *tmp;
+
+	tmp = (char *)memoryBlock;
+
+	i = 0;
+	while (i < size && tmp[i])
+	{
+		if ( tmp[i] == searchedChar )
+			return (&tmp[i]);
+		i++;
+	}
+	return (NULL);
+}
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main() {
+
+    char data[] = { 10, 20, 30, 40, 50, 60, 70, 80, 90, 100 };
+    const unsigned int size = 15;
+
+    // On recherche une valeur inhéxistante :
+    void * found = memchr( data, 57, size );
+    printf( "57 is %s\n", ( found != NULL ? "found" : "not found" ) );
+
+    // On recherche une valeur existante :
+    found = memchr( data, 53, size );
+    printf( "50 is %s\n", ( found != NULL ? "found" : "not found" ) );
+    if ( found != NULL ) {
+        printf( "La valeur à la position calculée est %d\n", *((char *) found) );
+    }
+
+    return EXIT_SUCCESS;
+}
--- ft_memmove.c
+++ ft_memmove.c
@@ -1,7 +1,45 @@
 #include "libft.h"
 #include <string.h>
 
-void *memmove(void *dest, const void *src, size_t n)
+void *ft_memmove(void *dst, const void *src, size_t n)
 {
-
+	if (!dst && !src)
+		return (NULL);
+	if (n == 0)
+		return (dst);
 }
+
+void	*ft_memcpy(void *dst, const void *src, size_t n)
+{
+	if (!dst && !src)
+		return (NULL);
+	if (n == 0)
+		return (dst);
+	while (n)
+	{
+		((char*)dst)[n - 1] = ((const char*)src)[n - 1];
+		n--;
+	}
+	return (dst);
+}
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define ELEMENT_COUNT 10
+
+int main()
+{
+	char str[100] = "jour";
+	char dest[100] = "bonjour";
+	char str3[100] = "jour";
+	char dest3[100] = "bonjour";
+	printf("ft_memcpy : %s\n", ft_memcpy(dest, str, 10));
+	char str2[100] = "jour";
+	char dest2[100] = "bonjour";
+	printf("memcpy : %s\n", memcpy(dest, str, 10));
+
+    return EXIT_SUCCESS;
+}
+
