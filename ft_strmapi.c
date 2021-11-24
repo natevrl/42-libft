948c81fc36473b6d71674ad8fbb56f4742e74df9
Nathan BENHADOUCHE <nbenhado@e2r11p15.clusters.42paris.fr>
2021-11-24 18:47:42 +0100

    on attaque les fd

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -1,12 +1,18 @@
 #include "libft.h"
 #include <stdlib.h>
+#include <stdio.h>
+
+char test(unsigned int index, char c)
+{
+	return c += 10;
+}
 
 char *ft_strmapi(char const *s, char (*f)(unsigned int, char))
 {
 	unsigned int i;
-	char const *new_str;
+	char *new_str;
 
-	new_str = malloc(sizeof(char const) * ft_strlen(s));
+	new_str = malloc(sizeof(char) * ft_strlen(s));
 	if (!new_str)
 		return (NULL);
 	i = 0;
@@ -15,4 +21,14 @@ char *ft_strmapi(char const *s, char (*f)(unsigned int, char))
 		new_str[i] = (*f)(i, s[i]);
 		i++;
 	}
+	new_str[i] = '\0';
+	return (new_str);
+}
+
+int main()
+{
+	char str[] = "aaaa";
+	printf("%s\n", str);
+	printf("%s\n", ft_strmapi(str, &test));
+	return (0);
 }
