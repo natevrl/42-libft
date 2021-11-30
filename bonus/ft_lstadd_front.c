c84b12b98a409f4b6f4c0b3f53aa6c39d41a6711
v3r <v3rslor@protonmail.com>
2021-11-30 19:21:38 +0100

    plus que 6 bonus

--- bonus/ft_lstadd_front.c
+++
@@ -1,6 +0,0 @@
-#include "libft.h"
-
-void ft_lstadd_front(t_list **alst, t_list *new)
-{
-	
-}
--- libft.h
+++ libft.h
@@ -43,6 +43,8 @@ size_t    ft_strlcat(char *dst, const char *src, size_t size);
 size_t    ft_strlcpy(char *dst, const char *src, size_t size);
 
 
+
+
 // Structure pour les bonus
 typedef struct s_list
 {
@@ -50,5 +52,15 @@ typedef struct s_list
 	struct s_list *next;
 } t_list;
 
+
+t_list *ft_lstnew(void *content);
+void ft_lstadd_front(t_list **alst, t_list *new);
+int ft_lstsize(t_list *lst);
+t_list *ft_lstlast(t_list *lst);
+void ft_lstadd_back(t_list **alst, t_list *new);
+void ft_lstdelone(t_list *lst, void (*del)(void *));
+void ft_lstclear(t_list **lst, void (*del)(void *));
+void ft_lstiter(t_list *lst, void (*f)(void *));
+t_list *ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
 #endif
 
