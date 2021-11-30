94289267a32ebff68c4c62b851bc3288425865e4
v3r <v3rslor@protonmail.com>
2021-11-30 17:46:12 +0100

    split enfin debugger

--- ft_split.c
+++ ft_split.c
@@ -12,68 +12,6 @@
 
 #include "libft.h"
 
-/*static int	count_words(const char *str, char c)
-{
-	int i;
-	int trigger;
-
-	i = 0;
-	trigger = 0;
-	while (*str)
-	{
-		if (*str != c && trigger == 0)
-		{
-			trigger = 1;
-			i++;
-		}
-		else if (*str == c)
-			trigger = 0;
-		str++;
-	}
-	return (i);
-}*/
-
-/*static char	*word_dup(const char *str, int start, int finish)
-{
-	char	*word;
-	int		i;
-
-	i = 0;
-	word = malloc((finish - start + 1) * sizeof(char));
-	while (start < finish)
-		word[i++] = str[start++];
-	word[i] = '\0';
-	return (word);
-}
-
-char		**ft_split(char const *s, char c)
-{
-	size_t	i;
-	size_t	j;
-	int		index;
-	char	**split;
-
-	if (!s || !(split = malloc((count_words(s, c) + 1) * sizeof(char *))))
-		return (0);
-	i = 0;
-	j = 0;
-	index = -1;
-	while (i <= ft_strlen(s))
-	{
-		if (s[i] != c && index < 0)
-			index = i;
-		else if ((s[i] == c || i == ft_strlen(s)) && index >= 0)
-		{
-			split[j++] = word_dup(s, index, i);
-			index = -1;
-		}
-		i++;
-	}
-	split[j] = 0;
-	return (split);
-}*/
-
-
 static char	*split_copy(char const *str, int debut, int taille)
 {
 	int		i;
@@ -117,7 +55,7 @@ static int	number_of_tabs(char const *str, char c)
 	{
 		if (str[i] != c)
 		{
-			i += next_c(str, i, c);
+			i += next_c(str, i, c) - 1;
 			number += 1;
 		}
 		i++;
@@ -140,35 +78,12 @@ char	**ft_split(char const *s, char c)
 	{
 		if (s[i] != c)
 		{
-			printf("%ld\n", i);
-			//printf("%d\n", next_c(s, i, c));
 			tab_de_tab[f] = split_copy(s, i, next_c(s, i, c));
-			f++;
 			i += next_c(s, i, c) - 1;
+			f++;
 		}
 		i++;
 	}
 	tab_de_tab[f] = 0;
 	return (tab_de_tab);
-}
-
-#include <stdio.h>
-
-int main()
-{
-	int i = 0;
-	char str[] = "lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultricies diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi."
-
-	char **tab;
-
-	printf("%d\n", number_of_tabs(str, 'z'));
-	//printf("%d\n", next_c(str, 5, '1'));
-	tab = ft_split(str, '1');
-	while (tab[i])
-	{
-		printf("tab[%d] : %s\n", i, tab[i]);
-		i++;
-	}
-    printf("%s",tab[i]);
-	return (0);
 }
