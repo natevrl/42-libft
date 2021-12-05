1e373054b7378ea5c03974dd706bafc9abd2e127
Nathan BENHADOUCHE <nbenhado@e2r12p16.clusters.42paris.fr>
2021-12-05 21:09:24 +0100

    strnstr corrige

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: nbenhado <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/26 16:34:19 by nbenhado          #+#    #+#             */
-/*   Updated: 2021/12/05 18:28:00 by nbenhado         ###   ########.fr       */
+/*   Updated: 2021/12/05 21:08:07 by nbenhado         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,22 +17,18 @@ char	*ft_strnstr(const char *big, const char *little, size_t len)
 	size_t	i;
 	size_t	y;
 
-	if (little == NULL || ft_strlen(little) == 0)
-		return ((char *)big);
 	i = 0;
-	while (big[i] && i < len)
+	if (little[i] == '\0')
+		return ((char *)big);
+	while (big[i] && i < len && (ft_strlen(big) >= ft_strlen(little)))
 	{
 		if (big[i] == little[0])
 		{
 			y = 0;
-			while (big[i] == little[y] && i < len)
-			{
-				if (little[y + 1] == '\0')
-					return ((char *)&big[i - y]);
+			while (big[i + y] == little[y] && little[y] && ((i + y) < len))
 				y++;
-				i++;
-			}
-			i--;
+			if (little[y] == '\0' || y == len)
+				return ((char *)(big + i));
 		}
 		i++;
 	}
