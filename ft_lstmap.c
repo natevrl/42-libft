db1d42d198db2a0e99525e295219e9ce6c93b07c
Nathan BENHADOUCHE <nbenhado@e3r13p10.clusters.42paris.fr>
2021-12-07 20:16:14 +0100

    map finit

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,31 +6,30 @@
 /*   By: nbenhado <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/07 13:30:53 by nbenhado          #+#    #+#             */
-/*   Updated: 2021/12/07 18:31:34 by nbenhado         ###   ########.fr       */
+/*   Updated: 2021/12/07 20:15:39 by nbenhado         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-t_list *ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
-	t_list *new;
-	
-	if (!lst)
-		return (NULL);
-	new = NULL;
-	while(lst->next)
+	t_list	*tmp;
+	t_list *begin;
+
+	if (!lst || !f || !del)
+		return (0);
+	begin = NULL;
+	while (lst)
 	{
-		new = ft_lstnew(f(lst->content)); 
-		if (!new)
+		tmp = ft_lstnew((*f)(lst->content));
+		if (!tmp)
 		{
-			ft_lstclear(&new, del);
-			return (NULL);
+			ft_lstclear(&tmp, del);
+			return (0);
 		}
-		new = new->next;
+		ft_lstadd_back(&begin, tmp);
 		lst = lst->next;
 	}
-	new = 0;
-	return (new);
-
+	return (begin);
 }
